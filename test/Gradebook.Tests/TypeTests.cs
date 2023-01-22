f66c1d106c8d27a9f0c1d22e8cb1dcb8e6513d35
Glareforme <91750407+Glareforme@users.noreply.github.com>
2023-01-22 21:31:02 +0200

    add unit tests with value and ref types

--- test/Gradebook.Tests/TypeTests.cs
+++ test/Gradebook.Tests/TypeTests.cs
@@ -4,7 +4,7 @@ namespace Gradebook.Tests;
 
 public class TypeUnitTests
 {
-
+    #region Tests
     [Fact]
     public void TheBooksReturnsDifferentObjects()
     {
@@ -41,12 +41,85 @@ public class TypeUnitTests
     }
 
     [Fact]
-    public void CanSetNewBookToOtherBook()
+    public void CantSetNewBookToOtherBook()
     {
         var book = GetBook("Test");
         SetBookToOtherBook(book, "New name");
 
         Assert.NotEqual("New name", book.Name);
+    }
+
+    public void CanSetNewBookToOtherBook()
+    {
+        var book = GetBook("Test");
+        SetBookToOtherBook(ref book, "New name");
+
+        Assert.Equal("New name", book.Name);
+    }
+
+    [Fact]
+    public void ValueTypesCantPassByValue()
+    {
+        var x = GetInt();
+        SetInt(x);
+
+        Assert.Equal(3, x);
+    }
+
+    [Fact]
+    public void ValueTypesCanPassByValue()
+    {
+        var x = GetInt();
+        SetInt(ref x);
+
+        Assert.Equal(32, x);
+    }
+
+    [Fact]
+    public void StringsBehaveLikeValueType()
+    {
+        string name = "Gaga";
+        TryMakeUpperCase(name);
+
+        Assert.NotEqual("GAGA", name);
+
+        var NewName = MakeUpperCase(name);
+
+        Assert.NotEqual("GAGA", name);
+        Assert.Equal("Gaga", name);
+        Assert.Equal("GAGA", NewName);
+    }
+    #endregion
+
+    #region Methods
+    private void TryMakeUpperCase(string param)
+    {
+        param.ToUpper();
+    }
+
+    private string MakeUpperCase(string param)
+    {
+        return param.ToUpper();
+    }
+
+    public int GetInt()
+    {
+        return 3;
+    }
+
+    public void SetInt(ref int integer)
+    {
+        integer = 32;
+    }
+
+    public void SetInt(int integer)
+    {
+        integer = 32;
+    }
+
+    private void SetBookToOtherBook(ref Book book, string newName)
+    {
+        book = new Book(newName);
     }
 
     private void SetBookToOtherBook(Book book, string newName)
@@ -63,4 +136,5 @@ public class TypeUnitTests
     {
         return new Book(name);
     }
+    #endregion
 }
